c1c65983a9d370cc5deeedecd979c3a4ce571c66
y12studio <y12studio@gmail.com>
2015-05-11 12:42:26 +0800

    fixed y12docker/fastwp:4.2.2 host_install.sh

--- fastwp/host_install.sh
+++ fastwp/host_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -v $(pwd)/fastwp:/fastwp y12docker/fastwp /start.sh
+docker run -v $(pwd)/fastwp:/fastwp y12docker/fastwp:4.2.2 /start.sh
 sudo chown -R "$USER" fastwp
 echo "===== AFTER INSTALL ====="
 echo "cd fastwp && source alias.sh"
