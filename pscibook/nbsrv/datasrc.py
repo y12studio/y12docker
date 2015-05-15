9385b364d13d56cd853cf427d0abd03185529576
y12studio <y12studio@gmail.com>
2015-05-15 17:18:28 +0800

    new bitcoin datasrc.py

---
+++ pscibook/nbsrv/datasrc.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def hello():
+    print("Hello World")
+
+#
+# import datasrc as dc
+# f=dc.read_bitcoin()
+# f[["Value","30MA","7MA"]].plot()
+#
+def read_bitcoin():
+    dataurl='https://www.quandl.com/api/v1/datasets/BCHAIN/MKPRU.csv?trim_start=2013-01-01&trim_end=2015-05-01'
+    df=pd.read_csv(dataurl)
+    df['7MA'] = pd.rolling_mean(df['Value'], 7)
+    df['30MA'] = pd.rolling_mean(df['Value'], 30)
+    df['Difference'] = df['Value'].diff()
+    return df
