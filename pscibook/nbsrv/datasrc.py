d90dfe772bd1b0ad5e27849bd4d35b032b886fd8
y12studio <y12studio@gmail.com>
2015-05-15 17:41:43 +0800

    pandas read_csv index date

--- pscibook/nbsrv/datasrc.py
+++ pscibook/nbsrv/datasrc.py
@@ -10,8 +10,9 @@ def hello():
 #
 def read_bitcoin():
     dataurl='https://www.quandl.com/api/v1/datasets/BCHAIN/MKPRU.csv?trim_start=2013-01-01&trim_end=2015-05-01'
-    df=pd.read_csv(dataurl)
+    df=pd.read_csv(dataurl,index_col='Date', parse_dates=True)
     df['7MA'] = pd.rolling_mean(df['Value'], 7)
     df['30MA'] = pd.rolling_mean(df['Value'], 30)
+    df['120MA'] = pd.rolling_mean(df['Value'], 120)
     df['Difference'] = df['Value'].diff()
     return df
